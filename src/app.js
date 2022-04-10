92fe80f05d1573aa8e3553d1272e05b3e6be91e2
Imran bin Ahmad Azhar <imranahmazh@gmail.com>
2022-04-10 22:40:35 +0100

    latest fixes

--- src/app.js
+++ src/app.js
@@ -3,59 +3,29 @@ App = {
 	loading: false,
 	contracts: {},
     
-	load: async() => {
-		await App.loadWeb3()
+	load: async () => {
+		//await App.loadWeb3()
 		await App.loadAccount()
 		await App.loadContract()
 		await App.render()
 	},
 
-	// https://medium.com/metamask/https-medium-com-metamask-breaking-change-injecting-web3-7722797916a8
-  	loadWeb3: async () => {
-    
 
-    	window.addEventListener('load', async () => {
-        // Modern dapp browsers...
-        if (window.ethereum) {
-            window.web3 = new Web3(ethereum);
-            console.log("Loaded....")
-            try {
-                // Request account access if needed
-                await ethereum.enable();
-                // Acccounts now exposed
-                web3.eth.sendTransaction({/* ... */});
-            } catch (error) {
-                // User denied account access...
-            }
-        }
-        // Legacy dapp browsers...
-        else if (window.web3) {
-            window.web3 = new Web3(web3.currentProvider);
-            // Acccounts always exposed
-            web3.eth.sendTransaction({/* ... */});
-        }
-        // Non-dapp browsers...
-        else {
-            console.log('Non-Ethereum browser detected. You should consider trying MetaMask!');
-        }
-        });
-    },
-
-	loadAccount: async() => {
+	loadAccount: async () => {
 
 		//const accounts=await web3.eth.getAccounts();
-		const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+		const accounts = await ethereum.request({ method: 'eth_accounts' });
 		App.account = accounts[0]
 
 		console.log(App.account);
 	},
 
 
-	loadContract: async () =>{
+	loadContract: async () => {
 		//var contract=require("truffle-contract")
 		const todoList = await $.getJSON('TodoList.json')
+
 		App.contracts.TodoList=TruffleContract(todoList)
-		//App.contracts.TodoList.setProvider(new Web3.providers.HttpProvider("http://127.0.0.1:7545"));
 		App.contracts.TodoList.setProvider(window.ethereum);
 		console.log(todoList)
 
@@ -63,7 +33,7 @@ App = {
 		App.todoList=await App.contracts.TodoList.deployed()
 	},
 
-	render: async()=>{
+	render: async () => {
 
 		//prevent double render
 		if (App.loading){
@@ -85,7 +55,7 @@ App = {
 
 	},
 
-	renderTasks: async() => {
+	renderTasks: async () => {
 		// Load tasks from blockchain
 		const taskCount = await App.todoList.taskCount()
 		const $taskTemplate = $('.taskTemplate')
@@ -93,6 +63,7 @@ App = {
 
 		// Render each tasks with a new task template
 		for (var i=1;i<=taskCount;i++){
+			// Fetch task data from blockchain
 			const task=await App.todoList.tasks(i)
 			const taskId=task[0].toNumber()
 			const taskContent=task[1]
@@ -102,8 +73,8 @@ App = {
 			const $newTaskTemplate = $taskTemplate.clone()
 			$newTaskTemplate.find('.content').html(taskContent)
 			$newTaskTemplate.find('input')
-							.prop('name',taskId)
-							.prop('name',taskCompleted)
+							.prop('name', taskId)
+							.prop('checked', taskCompleted)
 							.on('click', App.toggleCompleted)
 		
 
